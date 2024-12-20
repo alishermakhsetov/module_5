245f9b3aac75e36e98b6f393a946ba5c2b25ae73
Ashok93b <vepiga2549@eoilup.com>
2024-12-20 21:37:08 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,121 @@
+
+# pip freeze    -> kutibxonalar royhatini ko'rsatadi
+# pip install
+# pip freeze > requirements.txt     -> file ochib ichiga yozib berdadi kutibxonalarni
+# deactivate    -> activitni uzib beradi
+# python3 -m venv .venv     -> envarmit yaratib beradi
+# . .venv/bin/activate  -> aktivatsiya qilish
+# pip freeze > uninstall.txt    -> ktubxonalarni o'chirish uchun filege yozib olish
+# pip uninstall -r uninstall.txt  -> fileni o'chrib yuboradi
+# pip install -r requirements.txt   -> file ichidagini kutibxonani ornatish
+# pip uninstall -r requirements.txt     -> file ni o'chrib yuboradi
+# rm -rf venv3  -> ochgan venvni ochirish
+
+# alt + j   -> bittalab belgilash uchun
+# ctrl + shift + alt + j    -> hammasini belgilash uchun
+# ctrl + x ochirib nusqa oladi
+# ctrl + c butun qatorni nusqalaydi
+
+# ======================================================
+
+# github    -> malumotlar jamlanmasi markazi
+
+# paycharm -> github create repositories and push
+# terminal -> push github
+
+
+# =============================
+# terminal
+# mkdir UzumParser  -> file papka yartish proekt uchun
+# cd UzumParser/    -> papka ichiga kirish
+# ls -ll    -> ichini tekshirishUzumParser
+# python3 -m venv .venv     -> vertual boks yaratamiz
+# ls -a     -> yachrin fillarni korish
+# . .venv/bin/activate  -> aktivatsiya qilish
+# pip install redis -> nimadir ustanovka qilamiz
+# touch main.py     -> python file yaratamiz
+# nano main.py  -> python filega ichiga yozish uchun kirish
+# biror bir paython kod yozamiz
+# python3 main.py   -> run qilish
+# touch gitignore   -> file yaratib olamiz githubga joylamaydigan fillarni korsatish uchun
+# nano gitignore    -> file ichiga kiramiz
+# .venv papkani korsatamiz
+# .idea korsatamiz paycharimga tegishli papka
+# .env chaxsiy malumotlar
+# pip freeze tekshiramiz
+# pip freeze > requiremens.txt papkaga joylaymiz
+# cat requiremens.txt   -> ichini oqib olamiz
+
+# githubga yuborish uchun githubga kirib repositories ga kiramiz
+# yangi repositore yaratamiz
+# nom beramiz
+
+# git init                      -> papka yaratib beradi yaratmasa (sudo apt install git)
+# git add README.md             -> gitga qoshadigan narsalarni kiritamiz (main.py requiremens.txt) kop malumot bo'lsa
+#                                                                                                       ( . ) qoyamiz
+# git commit -m "first commit"  -> nima yaratganimiz haqida komentarya yozish
+# git branch -M main            -> branch yaratamiz defoultda main bo'ladi
+# git remote add origin https://github.com/Ashok93b/UzumParser.git  -> remod qilib korsatilgan linkni beramiz
+# git push -u origin main       -> berilgan nomni ishlatamiz
+
+# Shu komandalarni beradi
+
+# Username for 'https://github.com':    -> Username so'raydi
+# Password for 'https://Ashok93b@github.com':   -> parol so'raydi githubdan token olamiz
+
+
+# mkdir db          -> papka ochamiz
+# db/config.py      -> python file yaratamiz
+# nano config.py    -> ichiga kiramiz va birin narsa yozamis saqlab chiqib ketamiz
+# git add .         -> hamma file papkalarni ko'rib chiq
+# git status        -> holatni tekshrib koradi
+# git commit -m 'create config file'    -> komentarya qoshib qoyish
+# git status        -> qilsak hech narsa chiqmaydi,git edd qilganimiz saqlab turadi git commint upakovkaga o'raydi
+# git push -u origin main  -> nechta upakovka bo'lsa shunchasini jo'natadi
+
+# Username for 'https://github.com':    -> Username so'raydi
+# Password for 'https://Ashok93b@github.com':   -> parol so'raydi githubdan token olamiz
+
+
+
+
+
+
+# ******************************************************************
+# git config user.name "Alisher Maxsetov"
+# git config user.email "alisher.maxsetov.dev@gmail.com"
+# git config --global --list
+# Bu komandani berip qoymasa ishlamaydi
+
+
+
+
+
+
+
+# n = int(input())
+# temp = 0
+# count = 0
+#
+# while True:
+#     number = int(input())
+#     if temp + number >= n:
+#         break
+#     temp += number
+#     count += 1
+# print("Довольно!")
+# print(temp)
+# print(count)
+#
+# """
+# 2000
+# 900
+# 800
+# 700
+# 500
+# 200
+# 1000
+# """
+
+
+
